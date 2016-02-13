b7cf1e7700c6f941deb43a5ff63d01980ae20500
Choza973 <choza_castro@hotmail.com>
2016-02-13 13:51:37 -0600

    Update WSQ08.java

--- WSQ08.java
+++ WSQ08.java
@@ -1,3 +1,20 @@
-class WSQ08{
+class Pal{
+  public Pal(){
+  }
+  
+  public int isPalyndrome(){
+    
+  }
+  
+  public static void main(){
+    Pal number = new Pal();
+    number.isPalyndrome(1,200);
+  }
   
 }
+
+
+
+//Cosas que hacer:
+// 1 rango (1 a 200)
+//
