235dd214574a9269087f78f9a51bd7ba6b875154
César Augusto <cesarau.04@gmail.com>
2016-02-13 14:37:56 -0600

    Update WSQ08.java

--- WSQ08.java
+++ WSQ08.java
@@ -1,20 +1,28 @@
-class Pal{
-  public Pal(){
-  }
-  
-  public int isPalyndrome(){
-    
-  }
-  
-  public static void main(){
-    Pal number = new Pal();
-    number.isPalyndrome(1,200);
-  }
-  
+package wsq08;
+
+public class Palindrome {
+
+
+	public Palindrome(){
+	}
+	
+	public boolean isNatPal(){
+
+	}
+	
+	
+	public void MainFunction(int lowB, int uppB){
+		while (lowB <= uppB){
+			for (int cont=1; cont<31; cont++){
+				lowB.isNatPal();
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args){
+		Palindrome range = new Palindrome();
+		range.MainFunction(1,200);
+		
+	}
 }
-
-
-
-//Cosas que hacer:
-// 1 rango (1 a 200)
-//
